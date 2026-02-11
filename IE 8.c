2cc6ee1aed8e839f64f192841ac027f7f9cc85c8
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-11 14:06:21 +0530

    Create IE 8.c

---
+++ IE 8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%3==0 && a%7==0){
+        printf("Multiple of both 3 and 7");
+    }
+    else{
+        printf("Not multiple by bothe 3 and 7");
+    }
+    
+    return 0;
+}
