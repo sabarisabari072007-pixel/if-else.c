fed819b00f8ce8f956cb661724ac82cde2be9a9a
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-11 16:01:20 +0530

    Create IE 13.c

---
+++ IE 13.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c", &a);
+    if( a>=65&& a<=90)
+        printf("Uppercase Letter");
+    else
+        printf("Not Uppercase Letter");
+
+    return 0;
+}
