aa8f9c69c2ffcf64f01545d3df212769f8f2481a
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-11 14:45:48 +0530

    Create IE 10.c

---
+++ IE 10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+   scanf("%d",&a);
+   if(a>=100 && a<=999){
+       printf("Three-digit number");
+   }
+   else{
+       printf("Not a three digit number");
+   }
+    return 0;
+}
