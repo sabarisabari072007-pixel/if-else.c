a65ed6733cc8d41ae07bade7b1cbb6539b07cdf6
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-11 10:49:01 +0530

    Create IE 3 .c

---
+++ IE 3 .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%5==0){
+        printf("Divisible by 5");
+    }
+    else{
+        printf("Not divisible by 5");
+    }
+}
