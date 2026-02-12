7da728fa37678b4511db064e8ad01099f82b3c8a
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-12 10:03:04 +0530

    Create IE 19.c

---
+++ IE 19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=0 && a<=9){
+        printf("Digit");
+    }
+    else{
+        printf("Invaild");
+    }
+    return 0;
+}
