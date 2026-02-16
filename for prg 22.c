d6aa5e5f25729573a67668ea4ecb67f6ea615c1a
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-16 09:38:56 +0530

    Add files via upload

---
+++ for prg 22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int count=0;
+    for(int i=2;i<=n;i++){
+        int prime=1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+            prime=0;
+            break;
+        }
+        }
+        if(prime){
+         count++;
+    }
+    }
+    printf("%d",count);
+    return 0;
+}
